fb45c9980b36f533b24d90250d3e29629c496259
Bolatito Adeoye <titoadeoye00@gmail.com>
2022-09-04 09:23:51 -0800

    Add files via upload

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=egypt28/ml-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-api --image=$dockerpath 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-api 8080:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=egypt28/ml-api
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u egypt28
+docker tag ml-api $dockerpath:latest
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
